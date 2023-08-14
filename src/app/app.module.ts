bf2b2ebf35848adbb2edb6c1300ce25825caca5c
damm2001 <dianmart@espol.edu.ec>
2023-08-14 14:22:44 -0500

    Agregado nuevo código

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ReportComponent } from './pages/report/report.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ReportComponent,
+    AboutComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
